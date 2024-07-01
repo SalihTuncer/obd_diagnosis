57315a0d5848db32373c96824363e6a375ce29bf
Salih Tuncer <Salih.Tuncer98@gmail.com>
2024-07-01 16:09:38 +0200

    Project is structured object-oriented now. The connection to the car is now realizable via a synchronized and an asynchronized connection. Application os now configurable via settings.json. requirements.txt: Pydantic added.

---
+++ application/query_manager.py
@@ -0,0 +1,102 @@
+import time
+
+import obd
+
+
+class QueryManager:
+    def __init__(self, delay_cmds=2):
+
+        self._connect_sync()
+        self._connect_async(delay_cmds=delay_cmds)
+
+    def __del__(self):
+        if self.sync_conn.is_connected():
+            self.sync_conn.close()
+
+        if self.async_conn.is_connected():
+            self.async_conn.close()
+
+    def _connect_sync(self) -> None:
+        """
+            Connects to the OBD interface synchronously.
+
+        """
+        obd.logger.info('Connecting to the OBD interface synchronously.')
+
+        self.sync_conn = obd.OBD()
+
+    def _connect_async(self, delay_cmds=2) -> None:
+        """
+            Connects to the OBD interface asynchronously.
+
+            :param delay_cmds: The delay between commands
+        """
+        obd.logger.info('Connecting to the OBD interface asynchronously.')
+
+        self.async_conn = obd.Async(delay_cmds=delay_cmds)
+
+    def sync_queries(self, cmds: list[obd.OBDCommand]) -> None:
+        """
+            Queries the given connection for all available commands.
+
+            :param cmds: The commands to query
+            :return: None
+        """
+        if not self._is_sync_connected():
+            self._connect_sync()
+
+        for cmd in cmds:
+
+            resp = self.sync_conn.query(cmd.command)  # send the command, and parse the resp
+
+            if resp.is_null():
+                obd.logger.info(f'{cmd} is not supported.')
+                continue
+
+            obd.logger.info(f'{cmd.name}: {resp.value}')
+
+    def async_queries(self, cmds: list[obd.OBDCommand], duration=60) -> None:
+        """
+            Queries the given connection for all available commands asynchronously.
+
+            :param cmds: The commands to query
+            :param duration: The duration to query the commands for
+            :return: None
+        """
+        if not self._is_async_connected():
+            self._connect_async()
+
+        cmd: obd.OBDCommand
+        for cmd in cmds:
+            self.async_conn.watch(cmd.command, callback=self.cmd_to_cli)
+
+        self.async_conn.start()
+
+        time.sleep(duration)
+
+        self.async_conn.stop()
+
+    def cmd_to_cli(self, resp: obd.OBDResponse) -> None:
+        """
+            A callback function that prints the given OBDResponse.
+
+            :param resp: The OBDResponse to print
+            :return: None
+        """
+        obd.logger.info(f'{resp.command}: {resp.value}')
+
+    def _is_sync_connected(self) -> bool:
+        """
+            Returns whether the synchronous connection is connected.
+
+            :return: Whether the synchronous connection is connected
+        """
+        return self.sync_conn.is_connected()
+
+    def _is_async_connected(self) -> bool:
+        """
+            Returns whether the asynchronous connection is connected.
+
+            :return: Whether the asynchronous connection is connected
+        """
+        return self.async_conn.is_connected()
--- main.py
+++ main.py
@@ -1,180 +1,52 @@
 import json
+import os
 
 import obd
-import time
+
+from application.application import Application
+from data_models.settings import LogSettings, Settings
 
 
-def configure_logger(log_level: int | None = obd.logging.DEBUG):
+def load_settings(file: str) -> Settings:
+    """
+        Loads the settings from the given file.
+
+        :param file: The file to load the settings from
+        :return: The settings loaded from the file
+    """
+    with open(file, 'r') as f:
+        settings_json = json.load(f)
+
+        return Settings(**settings_json)
+
+
+def configure_logger(log_settings: LogSettings):
     """
         Configures the logger to print all debug information.
     """
-    if log_level:
-        obd.logger.setLevel(log_level)  # enables all debug information
+    obd.logger.setLevel(log_settings.logLevel)  # enables all debug information
 
-    # logs should be written to files too
-    obd.logger.addHandler(obd.logging.FileHandler('obd.log'))
+    # logs should be appended to files
+    obd.logger.addHandler(obd.logging.FileHandler(log_settings.logFile, mode='a', encoding='utf-8'))
 
-
-def save_all_commands(output_file: str) -> None:
-    """
-        Saves all the commands to a json file.
-    :param output_file: The file to save the commands to
-    :return: None
-    """
-
-    # make a copy of a list
-    modes = list(obd.commands.modes)
-
-    cmds = {}
-
-    mode_ctr = 0
-
-    for mode in modes:
-
-        mode_cmds = []
-
-        for cmd in mode:
-
-            if not cmd:
-                continue
-
-            cmd.header = cmd.header.decode('utf-8')
-            cmd.command = cmd.command.decode('utf-8')
-            del cmd.decode
-
-            mode_cmds.append(cmd.__dict__)
-
-        cmds[f'mode_{mode_ctr}'] = mode_cmds
-
-        mode_ctr += 1
-
-    # list to json file
-    with open(output_file, 'w') as f:
-        json.dump(cmds, f, indent=4)
-
-
-def save_all_supported_commands(connection: obd.OBD, output_file: str) -> None:
-    """
-        Saves all the commands to a json file.
-    :param output_file: The file to save the commands to
-    :return: None
-    """
-
-    # make a copy of a list
-    supported_cmds: list[obd.OBDCommand] = connection.supported_commands
-
-    cmds = []
-
-    for cmd in supported_cmds:
-
-        if not cmd:
-            continue
-
-        cmd.header = cmd.header.decode('utf-8')
-        cmd.command = cmd.command.decode('utf-8')
-        del cmd.decode
-
-        cmds.append(cmd.__dict__)
-
-    # list to json file
-    with open(output_file, 'w') as f:
-        json.dump(cmds, f, indent=4)
-
-
-def cmd_to_cli(resp: obd.OBDResponse) -> None:
-    """
-        A callback function that prints the given OBDResponse.
-
-        :param resp: The OBDResponse to print
-        :return: None
-    """
-    print(f'{resp.command}: {resp.value}')
-
-
-def sync_queries(connection: obd.OBD, cmds: dict[str, obd.OBDCommand]) -> None:
-    """
-        Queries the given connection for all available commands.
-
-        :param connection: The connection to query
-        :return: None
-    """
-    for naming, cmd in cmds.items():
-
-        resp = connection.query(cmd)  # send the command, and parse the resp
-
-        if resp.is_null():
-            print(f'{cmd} is not supported.')
-            continue
-
-        print(f'{naming}: {resp.value}')
-
-
-def async_queries(connection: obd.Async, cmds: dict[str, obd.OBDCommand], duration=60) -> None:
-    """
-        Queries the given connection for all available commands asynchronously.
-
-        :param connection: The connection to query
-        :return: None
-    """
-    for naming, cmd in cmds.items():
-        connection.watch(cmd, callback=cmd_to_cli)
-
-    connection.start()
-
-    time.sleep(duration)
-
-    connection.stop()
-
-
-mode_1_cmds = {
-    'Engine RPM': obd.commands['RPM'],
-    'Vehicle Speed': obd.commands['SPEED'],
-    'Throttle Position': obd.commands['THROTTLE_POS'],
-    'Relative throttle position': obd.commands['RELATIVE_THROTTLE_POS'],
-    'Engine Coolant Temperature': obd.commands['COOLANT_TEMP'],
-    'Intake Air Temp': obd.commands['INTAKE_TEMP'],
-    'Ambient air temperature': obd.commands['AMBIANT_AIR_TEMP'],
-    'Engine oil temperature	': obd.commands['OIL_TEMP'],
-    'Fuel Type': obd.commands['FUEL_TYPE'],
-    'Fuel System Status': obd.commands['FUEL_STATUS'],
-    'Engine fuel rate': obd.commands['FUEL_RATE'],
-    'Fuel Level Input': obd.commands['FUEL_LEVEL']
-}
-
-# mode_2_cmds = {
-#     for k, v in obd.commands.mode_1_cmds.items()
-# }
-
-# https://python-obd.readthedocs.io/en/latest/Command%20Tables/
-relevant_cmds = {
-    'Engine RPM': obd.commands.SPEED,
-    'Vehicle Speed': obd.commands.SPEED,
-    'Throttle Position': obd.commands.THROTTLE_POS,
-    'Relative throttle position': obd.commands.RELATIVE_THROTTLE_POS,
-    'Engine Coolant Temperature': obd.commands.COOLANT_TEMP,
-    'Intake Air Temp': obd.commands.INTAKE_TEMP,
-    'Ambient air temperature': obd.commands.AMBIANT_AIR_TEMP,
-    'Engine oil temperature	': obd.commands.OIL_TEMP,
-    'Fuel Type': obd.commands.FUEL_TYPE,
-    'Fuel System Status': obd.commands.FUEL_STATUS,
-    'Engine fuel rate': obd.commands.FUEL_RATE,
-    'Fuel Level Input': obd.commands.FUEL_LEVEL
-}
 
 if __name__ == "__main__":
-    configure_logger(log_level=None)
+    # load the settings
+    settings = load_settings('resource/settings.json')
 
-    sync_conn = obd.OBD()
+    # configure the logger
+    configure_logger(log_settings=settings.logging)
 
-    # save_all_supported_commands(sync_conn, 'supported_cmds.json')
+    app = Application(settings)
 
-    # resp = sync_conn.query(obd.commands.GET_DTC)  # send the command, and parse the resp
+    # Export all commands to a JSON if not already done
+    if not os.path.exists(settings.commandos.allCommandos):
+        app.command_helper.save_all_commands()
 
-    # print(f'{resp.value}')
+    # Export all supported commands to a JSON if not already done
+    if not os.path.exists(settings.commandos.supportedCommandos):
+        app.command_helper.save_all_supported_commands(
+            connection=app.get_sync_connection()
+        )
 
-    # sync_queries(sync_conn, relevant_cmds)
-
-    async_conn = obd.Async(delay_cmds=2)
-
-    # async_queries(async_conn, mode_1_cmds)
-    print()
+    app.run()
